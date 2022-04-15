2ce0daf4b29ebf1aaae3c62c71cc59b54a74f55d
nizarkadri <nizarkadri8@gmail.com>
2022-04-15 02:46:27 -0400

    initial commit

--- mysiteS21/settings.py
+++ mysiteS21/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tot49e**9&f-i92v9*i)@7n789p$r_dzre-++0)p^qo_56!*20'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
